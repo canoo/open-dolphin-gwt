85187f482bc8bc558b57ac8bbd77d7bf8a9cce3d
Sven Ehrke <sven.ehrke@canoo.com>
2014-11-25 16:54:17 +0100

    reverted to orginal version coming from OpenDolphin project

--- src/main/resources/com/canoo/opendolphin/public/opendolphin.js
+++ src/main/resources/com/canoo/opendolphin/public/opendolphin.js
@@ -1113,18 +1113,7 @@ var opendolphin;
             var model = new opendolphin.ClientPresentationModel(id, type);
             if (attributes && attributes.length > 0) {
                 attributes.forEach(function (attribute) {
-                    //ORG: model.addAttribute(attribute);
-
-                    //OD-GWT: start change:
-                    if (attribute.constructor.name == 'Array') {
-                        attribute.forEach(function(elem) {
-                            model.addAttribute(elem);
-                        })
-                    } else {
-                        model.addAttribute(attribute);
-                    }
-                    //OD-GWT: end change.
-
+                    model.addAttribute(attribute);
                 });
             }
             this.getClientModelStore().add(model);
