ae6ce183c0f951be83f38b83c3d443d0f5e39187
Sven Ehrke <sven.ehrke@canoo.com>
2015-09-14 16:24:32 +0200

    upgrade to OpenDolphinJS version '1.0-RC2B3'

--- src/main/java/com/canoo/opendolphin/client/gwt/DolphinBuilder.java
+++ src/main/java/com/canoo/opendolphin/client/gwt/DolphinBuilder.java
@@ -5,11 +5,15 @@ public class DolphinBuilder {
 	String url;
 	boolean reset;
 	int slackMS;
+	int maxBatchSize;
+	boolean supportCORS;
 	DolphinErrorHandler dolphinErrorHandler;
 
 	public DolphinBuilder() {
 		reset = false;
 		slackMS = 300;
+		maxBatchSize = 50;
+		supportCORS = false;
 	}
 
 	public DolphinBuilder url(String url) {
@@ -24,15 +28,23 @@ public class DolphinBuilder {
 		this.slackMS = slackMS;
 		return this;
 	}
+	public DolphinBuilder maxBatchSize(int maxBatchSize) {
+		this.maxBatchSize = maxBatchSize;
+		return this;
+	}
+	public DolphinBuilder supportCORS(boolean supportCORS) {
+		this.supportCORS = supportCORS;
+		return this;
+	}
 	public DolphinBuilder errorHandler(DolphinErrorHandler dolphinErrorHandler) {
 		this.dolphinErrorHandler = dolphinErrorHandler;
 		return this;
 	}
 
 	public final ClientDolphin build() {
-		return build_(url, reset, slackMS, dolphinErrorHandler);
+		return build_(url, reset, slackMS, maxBatchSize, supportCORS, dolphinErrorHandler);
 	}
-	public final native ClientDolphin build_(String url, boolean reset, int slackMS, DolphinErrorHandler errorHandler) /*-{
+	public final native ClientDolphin build_(String url, boolean reset, int slackMS, int maxBatchSize, boolean supportCORS, DolphinErrorHandler errorHandler) /*-{
 		var eh = function(evt) {
 			if (errorHandler) {
 				var de = @com.canoo.opendolphin.client.gwt.DolphinErrorEvent::new(Ljava/lang/String;Ljava/lang/String;ILjava/lang/String;)(evt.kind, evt.url, evt.httpStatus, evt.message);
@@ -43,7 +55,7 @@ public class DolphinBuilder {
 			}
 
 		};
-		return $wnd.opendolphin.makeDolphin().url(url).reset(reset).slackMS(slackMS).errorHandler(eh).build();
+		return $wnd.opendolphin.makeDolphin().url(url).reset(reset).slackMS(slackMS).errorHandler(eh).supportCORS(supportCORS).build();
 	}-*/;
 
 }
